f32b49217abb9f37f816ea556e46a8e31a2711f4
Палыч <samsonovegor037@gmail.com>
2025-09-29 20:50:52 +0300

    zadanie 3.3.2

---
+++ zadanie 3.3.2/zadanie 3.3.2/Program.cs
@@ -0,0 +1,8 @@
+﻿string myName = "Egor";
+Console.WriteLine(myName);
+int Age = 19;
+Console.WriteLine("my age is {0}", Age);
+bool Pet = true;
+Console.WriteLine("Do I have a pet? {0}", Pet);
+double Size = 43.3;
+Console.WriteLine("My shoe size is {0}", Size);
